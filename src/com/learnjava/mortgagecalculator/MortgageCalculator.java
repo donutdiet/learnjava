9d83c4a90d493dca5adc37494bce33781fe7992d
donutdiet <jerrywang0404@gmail.com>
2022-01-22 14:56:50 -0500

    Close the scanner to precent resource leak

--- src/com/learnjava/mortgagecalculator/MortgageCalculator.java
+++ src/com/learnjava/mortgagecalculator/MortgageCalculator.java
@@ -2,6 +2,7 @@ package com.learnjava.mortgagecalculator;
 
 import java.text.NumberFormat;
 import java.util.Scanner;
+
 /*
    The formula for Mortgage Payment is as follows:
    M = P [{r(1+r)^n}/{(1+r)^n – 1}]
@@ -23,18 +24,20 @@ public class MortgageCalculator {
         double principle = reader.nextDouble();
 
         System.out.print("Annual Interest Rate as decimal: ");
-            double rate = reader.nextDouble();
-        double monthlyRate = rate/12;
+        double rate = reader.nextDouble();
+        double monthlyRate = rate / 12;
 
         System.out.print("Period (Years): ");
         int months = reader.nextInt();
         months *= 12;
 
-        double mortgage = principle*monthlyRate;
-        mortgage *= Math.pow(1+monthlyRate,  months);
-        mortgage /= (Math.pow(1+monthlyRate, months)-1);
+        double mortgage = principle * monthlyRate;
+        mortgage *= Math.pow(1 + monthlyRate, months);
+        mortgage /= (Math.pow(1 + monthlyRate, months) - 1);
 
         NumberFormat currency = NumberFormat.getCurrencyInstance();
         System.out.println("Mortgage: " + currency.format(mortgage));
+
+        reader.close();
     }
 }
