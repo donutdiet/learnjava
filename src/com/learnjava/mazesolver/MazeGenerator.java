7df842c184f33a267d8d9e864d64a6245fb28ef1
donutdiet <jerrywang0404@gmail.com>
2022-01-22 14:47:54 -0500

    Test commit in vs code

--- src/com/learnjava/mazesolver/MazeGenerator.java
+++ src/com/learnjava/mazesolver/MazeGenerator.java
@@ -4,15 +4,15 @@ public class MazeGenerator {
 
     public static void main(String[] args) {
         generateMaze();
-
-
+        // test how to commit changes in vs code to github
     }
 
-    //  m: between 2 and 10
-    //  n: between 2 and 10
-    //  maze elements is either 1 or 0, but there is a "2" at one place
-    //  m.path = new LinkedList<>()
-    //  m.start: randomly pick a position, but you need to do boundary check.  start is between the min and max value of both m and n
+    // m: between 2 and 10
+    // n: between 2 and 10
+    // maze elements is either 1 or 0, but there is a "2" at one place
+    // m.path = new LinkedList<>()
+    // m.start: randomly pick a position, but you need to do boundary check. start
+    // is between the min and max value of both m and n
     public static Maze generateMaze() {
         Maze m = new Maze();
         return m;
