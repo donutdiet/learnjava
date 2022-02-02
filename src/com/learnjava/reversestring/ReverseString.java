dc39a1d388ff6a3bc191ebffa19cdf3fd0f97e60
donutdiet <jerrywang0404@gmail.com>
2022-02-01 20:53:23 -0500

    reverse string solution

--- src/com/learnjava/reversestring/ReverseString.java
+++ src/com/learnjava/reversestring/ReverseString.java
@@ -1,18 +1,36 @@
 package com.learnjava.reversestring;
 
+import javax.sound.sampled.SourceDataLine;
+
 /**
- * This program reverses every word of a string and display the reversed string as an output. For example,
- * if we input a string as “Reverse the word of this string” then the output of the program
+ * This program reverses every word of a string and display the reversed string
+ * as an output. For example,
+ * if we input a string as “Reverse the word of this string” then the output of
+ * the program
  * would be: “esrever eht drow fo siht gnirts”.
  */
 public class ReverseString {
 
-    public void reverseWordInMyString(String str) {
+    public String reverseWord(String word) {
+        String result = "";
+        for (int j = 0; j < word.length(); j++) {
+            result += word.charAt(word.length() - j - 1);
+        }
+        return result;
+    }
 
+    public String reverseWordInMyString(String str) {
+        String[] words = str.split(" ");
+        String result = "";
+        for (int i = 0; i < words.length; i++) {
+            String reversedWord = reverseWord(words[i]);
+            result += reversedWord + " ";
+        }
+        return result;
     }
 
     public static void main(String[] args) {
         ReverseString rs = new ReverseString();
-        rs.reverseWordInMyString("Reverse every word in this string");
+        System.out.println(rs.reverseWordInMyString("Reverse every word in this string"));
     }
 }
