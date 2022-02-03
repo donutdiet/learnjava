ecebf1d9ee72fd423617cb42829c49573136e911
donutdiet <jerrywang0404@gmail.com>
2022-02-03 17:20:23 -0500

    Multiplication Table Skeleton Created

---
+++ src/com/learnjava/multiplicationtable/MultiplicationTable.java
@@ -0,0 +1,19 @@
+package com.learnjava.multiplicationtable;
+
+/**
+ * to generate a multiplication table of up to size 9 * 9
+ */
+public class MultiplicationTable {
+
+
+    public void printMultiplicationTable(int tableSize) {
+
+    }
+
+    public static void main(String[] args) {
+        MultiplicationTable mt = new MultiplicationTable();
+        mt.printMultiplicationTable(3);
+        mt.printMultiplicationTable(5);
+        mt.printMultiplicationTable(9);
+    }
+}
