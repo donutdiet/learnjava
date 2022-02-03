284049ff8dcbe311c18ebff6431f9279c1e33f4a
donutdiet <jerrywang0404@gmail.com>
2022-02-03 17:10:11 -0500

    prime number checker solution

--- src/com/learnjava/primechecker/PrimeNumberChecker.java
+++ src/com/learnjava/primechecker/PrimeNumberChecker.java
@@ -11,12 +11,17 @@ public class PrimeNumberChecker {
     // if i % j == 0, i is not a prime number
     // otherwise i is a prime number
     public boolean checkPrimeNumber(int num) {
-        return false;
+        for (int i = 2; i < num / 2; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) {
         PrimeNumberChecker pmc = new PrimeNumberChecker();
-        pmc.checkPrimeNumber(71);
-        pmc.checkPrimeNumber(100);
+        System.out.println(pmc.checkPrimeNumber(71));
+        System.out.println(pmc.checkPrimeNumber(100));
     }
 }
