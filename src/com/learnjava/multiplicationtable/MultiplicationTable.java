f1afaceed97e2fd5d5c4cf179f359359688c5fbf
donutdiet <jerrywang0404@gmail.com>
2022-02-05 19:29:34 -0500

    multiplication table for any number 1-9

--- src/com/learnjava/multiplicationtable/MultiplicationTable.java
+++ src/com/learnjava/multiplicationtable/MultiplicationTable.java
@@ -5,9 +5,28 @@ package com.learnjava.multiplicationtable;
  */
 public class MultiplicationTable {
 
-
     public void printMultiplicationTable(int tableSize) {
-
+        for (int i = 0; i <= tableSize; i++) {
+            if (i == 0) {
+                System.out.print("        ");
+                continue;
+            }
+            System.out.printf("%4d", i);
+        }
+        System.out.print("\n");
+        System.out.println("-".repeat(tableSize * 4 + 10));
+        for (int i = 1; i <= tableSize; i++) {
+            for (int j = 0; j <= tableSize; j++) {
+                if (j == 0) {
+                    System.out.printf("%4d", i);
+                    System.out.print("   |");
+                    continue;
+                }
+                System.out.printf("%4d", i * j);
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 
     public static void main(String[] args) {
