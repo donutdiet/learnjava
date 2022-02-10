387fa8e32a04f28712048110fb2b60c9610f97d9
donutdiet <jerrywang0404@gmail.com>
2022-02-09 20:04:34 -0500

    majorityelement solution with hashmap

--- src/com/learnjava/leetcode/majorityelement169/MajorityElement.java
+++ src/com/learnjava/leetcode/majorityelement169/MajorityElement.java
@@ -1,11 +1,14 @@
 package com.learnjava.leetcode.majorityelement169;
 
+import java.util.HashMap;
+
 /**
  * Leetcode #169
  *
  * Given an array nums of size n, return the majority element.
  *
- * The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+ * The majority element is the element that appears more than ⌊n / 2⌋ times. You
+ * may assume that the majority element always exists in the array.
  *
  * * Example 1:
  * Input: nums = [3,2,3]
@@ -18,18 +21,30 @@ package com.learnjava.leetcode.majorityelement169;
 public class MajorityElement {
 
     public int majorityElement(int[] nums) {
-        return 0;
+        HashMap<Integer, Integer> frequencyMap = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            if (!frequencyMap.containsKey(nums[i])) {
+                frequencyMap.put(nums[i], 1);
+            } else {
+                int count = frequencyMap.get(nums[i]);
+                frequencyMap.replace(nums[i], count + 1);
+                if (count + 1 > nums.length / 2) {
+                    return nums[i];
+                }
+            }
+        }
+        return -1;
     }
 
     public static void main(String[] args) {
         MajorityElement me = new MajorityElement();
-        int[] a = {3,2,3};
+        int[] a = { 3, 2, 3 };
         System.out.println(me.majorityElement(a));
 
-        int[] b = {2,2,1,1,1,2,2};
+        int[] b = { 2, 2, 1, 1, 1, 2, 2 };
         System.out.println(me.majorityElement(b));
 
-        int[] c = {1 , 1 , 2 , 2 , 1 , 2 , 2};
+        int[] c = { 1, 1, 2, 2, 1, 2, 2 };
         System.out.println(me.majorityElement(c));
     }
 }
