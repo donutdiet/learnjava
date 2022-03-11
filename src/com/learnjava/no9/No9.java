fab04bc08023751f6c0ab5409c78fc08979afbcd
donutdiet <jerrywang0404@gmail.com>
2022-03-10 23:45:30 -0500

    no9 requirement added

---
+++ src/com/learnjava/no9/No9.java
@@ -0,0 +1,26 @@
+package com.learnjava.no9;
+
+import java.util.List;
+
+/**
+ * Given a list of non-negative integers, return a list of those numbers except omitting
+ * any that end with 9
+ *
+ * no9([1,2,19]) -> [1,2]
+ * no9([9,19,29,3]) -> [3]
+ * no9([1,2,3]) -> [1,2,3]
+ *
+ * Try not to copy/crate a new list, do the transformation in-place.
+ * You can come up with two methods - in place one and new list one
+ */
+public class No9 {
+
+    public static List<Integer> remove9(List<Integer> numList) {
+        return numList;
+    }
+    public static void main(String[] args) {
+        System.out.println(remove9(List.of(1,2,19)));
+        System.out.println(remove9(List.of(9,19,29,3)));
+        System.out.println(remove9(List.of(1,2,3)));
+    }
+}
