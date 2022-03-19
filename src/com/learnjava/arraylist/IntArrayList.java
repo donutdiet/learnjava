9cb31b0c60f28ff62e83791f605556cbd57bae66
donutdiet <jerrywang0404@gmail.com>
2022-03-18 21:43:37 -0400

    ArrayList requirement

---
+++ src/com/learnjava/arraylist/IntArrayList.java
@@ -0,0 +1,103 @@
+package com.learnjava.arraylist;
+
+// my implementation of java int ArrayList.  I have not learned Generics yet, so...
+public class IntArrayList {
+
+    /**
+     * Returns the number of elements in this list.
+     */
+    public int size() {
+        return 0;
+    }
+
+    /**
+     * Returns true if this list contains the specified element.
+     */
+    public boolean isEmpty() {
+        return true;
+    }
+
+    /**
+     * Returns the index of the first occurrence of the specified element
+     * in this list, or -1 if this list does not contain the element.
+     * or -1 if there is no such index.
+     */
+    public int indexOf(int num) {
+        return -1;
+    }
+
+    /**
+     * Returns the index of the last occurrence of the specified element
+     * in this list, or -1 if this list does not contain the element.
+     */
+    public int lastIndexOf(int num) {
+        return -1;
+    }
+
+    /**
+     * Returns the element at the specified position in this list.
+     */
+    public int get(int index) {
+        return -1;
+    }
+
+    /**
+     * Replaces the element at the specified position in this list with
+     * the specified element.
+     */
+    public void set(int index, int num) {
+
+    }
+
+    /**
+     * Appends the specified element to the end of this list.
+     */
+    public boolean add(int num) {
+        return true;
+    }
+
+    /**
+     * Inserts the specified element at the specified position in this
+     * list. Shifts the element currently at that position (if any) and
+     * any subsequent elements to the right (adds one to their indices).
+     */
+    public boolean add(int index, int num) {
+        return true;
+    }
+
+    /**
+     * Removes the element at the specified position in this list.
+     * Shifts any subsequent elements to the left (subtracts one from their
+     * indices).
+     *
+     * @param index the index of the element to be removed
+     * @return the element that was removed from the list
+     * @throws IndexOutOfBoundsException {@inheritDoc}
+     */
+    public boolean remove(int index) {
+        return true;
+    }
+
+    /**
+     * Removes all of the elements from this list.  The list will
+     * be empty after this call returns.
+     */
+    public void clear() {
+
+    }
+
+    /**
+     * Appends all of the elements in the specified collection to the end of
+     * this list, in the order that they are returned by the
+     * specified collection's Iterator.  The behavior of this operation is
+     * undefined if the specified collection is modified while the operation
+     * is in progress.  (This implies that the behavior of this call is
+     * undefined if the specified collection is this list, and this
+     * list is nonempty.)
+     */
+    public void addAll(int[] nums) {
+
+    }
+
+
+}
